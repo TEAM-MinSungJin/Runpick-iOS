a898caa55161a21a20fcefec6bd792180273af62
minaje0917 <k050918@naver.com>
2024-09-26 09:47:43 +0900

    Feature: Project Module Setting

---
+++ Projects/ThridPartyLib/Project.swift
@@ -0,0 +1,15 @@
+//
+//  Project.swift
+//  ProjectDescriptionHelpers
+//
+//  Created by 선민재 on 9/26/24.
+//
+
+import ProjectDescription
+import ProjectDescriptionHelpers
+
+let project = Project.makeModule(
+    name: "ThridPartyLib",
+    product: .staticFramework,
+    dependencies: []
+)
